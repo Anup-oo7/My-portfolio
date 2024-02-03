ebac0e779c327eff638ea5ee84e5363679a1be55
Anup-oo7 <srivastava8281@gmail.com>
2024-02-03 16:02:04 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const projects = [
+  {
+    title: "BMI Tracker",
+    subtitle: "MERN Stack",
+    description:
+    "BMI Tracker is a web app based on MERN technologies where user can create account and they can check their BMI and its risk management and they can easily track their previous BMIs.",
+
+    image: "./project-1.gif",
+    // link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Ek link",
+    subtitle: "MERN Stack",
+    description:
+    'Ek link is a web app where user will create account and make their profile which includes theme selection, and their profession. It will help the user to keep their links at one place whether social media links or business links and these links can be easily shared. ',
+    image: "./project-2.gif",
+    // link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Dynamic Form",
+    subtitle: "MERN stack",
+    description:
+    'It is a kind of form which can be created according to the required questions for the user. It is currently made for the purpose of loan application.',
+    image: "./project-3.gif",
+    // link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "HTML-CSS & JavaScript",
+    description:
+    'This is an amazing to do list web app where user can add, edit or remove todos. It has amazing UI and user friendly.',
+    image: "./project-4.gif",
+    // link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React Js",
+  "Node Js",
+  "Express js",
+  "Mongo DB",
+  "Material UI",
+  "BootStrap",
+  "HTML & CSS"
+
+];
